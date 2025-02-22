d8d6ac54466df28bf84c0f26ec3c5bcbe58b04e5
marceloremeseiro <marcelo.remeseiro@gmail.com>
2025-02-22 18:42:56 +0100

    mas errores

--- next.config.js
+++ next.config.js
@@ -8,33 +8,6 @@ const nextConfig = {
   output: 'standalone',
   env: {
     PORT: "1001"
-  },
-  // Forzar modo dinámico
-  dynamicParams: true,
-  // Deshabilitar la generación estática
-  staticPageGenerationTimeout: 0,
-  generateStaticParams: false,
-  webpack: (config, { isServer }) => {
-    // Ignorar módulos específicos que no son necesarios
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      'cpu-features': false,
-      './crypto/build/Release/sshcrypto.node': false
-    };
-
-    return config;
-  },
-  // Ignorar errores de ESLint en producción
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Ignorar errores de TypeScript en producción
-    ignoreBuildErrors: true,
-  },
-  // Deshabilitar la generación estática de la página 404
-  staticPages: {
-    '/404': false
   }
 }
 
